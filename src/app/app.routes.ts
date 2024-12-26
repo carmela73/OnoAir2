ff35a820ab46a54e26b500d35a93c8ace5b8f602
Carmela <carmellush@gmail.com>
2024-12-26 21:43:32 +0200

    book-flight component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { provideRouter, Routes } from '@angular/router';
 import { HomePageComponent } from './features/home-page/home-page.component';
 import { FlightsTableComponent } from './features/flights/pages/flights-table/flights-table.component'; // טבלת הטיסות
+import { BookFlightComponent } from './features/flights/pages/book-flight/book-flight.component'; // נתיב מעודכן
 
 export const routes: Routes = [
     { path: '', component: HomePageComponent }, // דף הבית
     { path: 'find-flight', component: FlightsTableComponent},
+    { path: 'book/:flightNumber', component: BookFlightComponent }, //דף הזמנת טיסה
     { path: '**', redirectTo: '' } // הפניה לנתיב ברירת מחדל
 ];
 
---
+++ src/app/features/flights/pages/book-flight/book-flight.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FlightService } from '../../service/flight.service';
+import { Flight } from '../../model/flight.model';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-book-flight',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './book-flight.component.html',
+  styleUrls: ['./book-flight.component.css']
+})
+
+export class BookFlightComponent implements OnInit {
+  flight?: Flight;
+
+  constructor(
+    private route: ActivatedRoute,
+    private flightService: FlightService
+  ) {}
+
+  ngOnInit(): void {
+    // קבלת מספר הטיסה מהנתיב
+    const flightNumber = this.route.snapshot.paramMap.get('flightNumber');
+    if (flightNumber) {
+      this.flight = this.flightService.get(flightNumber);
+    }
+  }
+}
