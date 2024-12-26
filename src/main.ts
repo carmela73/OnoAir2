5eeae5b74e42f6efaaa4d2eecd454b2a6d3ff001
Carmela <carmellush@gmail.com>
2024-12-26 16:44:20 +0200

    view home-page

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { appRouterProviders } from './app/app.routes'; 
+import { importProvidersFrom } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { appRouterProviders } from './app/app.routes';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(BrowserAnimationsModule),
+    ...appRouterProviders
+  ]
+}).catch(err => console.error(err));
