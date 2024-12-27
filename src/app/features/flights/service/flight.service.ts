6b41f07d384ed09845cc50e4cfaab901d965cceb
Carmela <carmellush@gmail.com>
2024-12-27 19:31:27 +0200

    update flight service

--- src/app/features/flights/service/flight.service.ts
+++ src/app/features/flights/service/flight.service.ts
@@ -14,17 +14,17 @@ export class FlightService {
 
   // טיסות סטטיות עם מקורות מגוונים
   private staticFlights: Omit<Flight, 'boardingDate' | 'arrivalDate'>[] = [
-    { flightNumber: "W61283", origin: "Tel Aviv", destination: "Krakow", numberOfSeats: 200, imageUrl: "https://www.elal.com/magazine/wp-content/uploads/2023/09/krakow-rynek-glowwny-806x463-shutterstock_1341413513.jpg"},
-    { flightNumber: "LX8396", origin: "Zurich", destination: "Frankfurt", numberOfSeats: 150, imageUrl: "https://www.elal.com/magazine/wp-content/uploads/2017/01/shutterstock_145475239.jpg"},
-    { flightNumber: "BA2189", origin: "London", destination: "Tel Aviv", numberOfSeats: 180, imageUrl: "https://www.zmantelaviv.com/wp-content/uploads/2023/09/2.jpg"},
-    { flightNumber: "LH7890", origin: "Berlin", destination: "Vienna", numberOfSeats: 170, imageUrl: "https://www.elal.com/magazine/wp-content/uploads/2017/05/shutterstock_238923085-1.jpg"},
-    { flightNumber: "AF3056", origin: "Paris", destination: "Rome", numberOfSeats: 190, imageUrl: "https://www.elal.com/magazine/wp-content/uploads/2017/01/shutterstock_147643964.jpg"},
-    { flightNumber: "IB2045", origin: "Madrid", destination: "Barcelona", numberOfSeats: 160, imageUrl: "https://www.elal.com/magazine/wp-content/uploads/2017/01/shutterstock_229604983.jpg"},
-    { flightNumber: "KL4510", origin: "Amsterdam", destination: "Athens", numberOfSeats: 200, imageUrl: "https://www.elal.com/magazine/wp-content/uploads/2018/07/shutterstock_776615074.jpg"},
-    { flightNumber: "AZ1198", origin: "Rome", destination: "Zurich", numberOfSeats: 210, imageUrl: "https://www.easygo.co.il/clients/easygo/gallery/Zurich/zuirch-1-756.jpg"},
-    { flightNumber: "OA3001", origin: "Athens", destination: "London", numberOfSeats: 140, imageUrl: "https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcRj8g2WGx2WKl5DjBWBUSrjKrxZ9s4_PGHPlZJi7Cth748HVkEG6BvXcPFQ7uyLUJfGmlKic1YO_LCZ7jepcZH2C5bUFSt2cpHY0uEIjA"},
-    { flightNumber: "OS9003", origin: "Vienna", destination: "Tokyo", numberOfSeats: 180, imageUrl: "https://blog.easygo.co.il/wp-content/uploads/2019/09/tokyo2.jpg"}
-  ];
+  new Flight('W61283', 'Tel Aviv', 'Krakow', new Date(), new Date(), 200, 'https://www.elal.com/magazine/wp-content/uploads/2023/09/krakow-rynek-glowwny-806x463-shutterstock_1341413513.jpg'),
+  new Flight('LX8396', 'Zurich', 'Frankfurt', new Date(), new Date(),  150, 'https://www.elal.com/magazine/wp-content/uploads/2017/01/shutterstock_145475239.jpg'),
+  new Flight('BA2189',  'London',  'Tel Aviv', new Date(), new Date(),  180,  'https://www.zmantelaviv.com/wp-content/uploads/2023/09/2.jpg'),
+  new Flight('LH7890',  'Berlin',  'Vienna', new Date(), new Date(),  170,  'https://www.elal.com/magazine/wp-content/uploads/2017/05/shutterstock_238923085-1.jpg'),
+  new Flight('AF3056',  'Paris',  'Rome', new Date(), new Date(),  190,  'https://www.elal.com/magazine/wp-content/uploads/2017/01/shutterstock_147643964.jpg'),
+  new Flight('IB2045',  'Madrid',  'Barcelona', new Date(), new Date(),  160,  'https://www.elal.com/magazine/wp-content/uploads/2017/01/shutterstock_229604983.jpg'),
+  new Flight('KL4510',  'Amsterdam', 'Athens', new Date(), new Date(), 200, 'https://www.elal.com/magazine/wp-content/uploads/2018/07/shutterstock_776615074.jpg'),
+  new Flight('AZ1198',  'Rome',  'Zurich', new Date(), new Date(),  210,  'https://www.easygo.co.il/clients/easygo/gallery/Zurich/zuirch-1-756.jpg'),
+  new Flight('OA3001',  'Athens',  'London', new Date(), new Date(),  140,  'https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcRj8g2WGx2WKl5DjBWBUSrjKrxZ9s4_PGHPlZJi7Cth748HVkEG6BvXcPFQ7uyLUJfGmlKic1YO_LCZ7jepcZH2C5bUFSt2cpHY0uEIjA'),
+  new Flight('OS9003',  'Vienna',  'Tokyo', new Date(), new Date(),  180,  'https://blog.easygo.co.il/wp-content/uploads/2019/09/tokyo2.jpg'),
+];
 
   // מתודה ליצירת תאריכים דינאמיים
   private generateDynamicDates(): void {
