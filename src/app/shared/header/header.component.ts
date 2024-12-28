5428e77ce044c9b91202e0f547437f89499da6eb
Carmela <carmellush@gmail.com>
2024-12-28 12:50:14 +0200

    update style

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -3,11 +3,12 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatMenuModule, MatIconModule, RouterModule, MatButtonModule],
+  imports: [MatMenuModule, MatIconModule, RouterModule, MatButtonModule, MatToolbarModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
