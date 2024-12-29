f26440b476c0689ca8684cd25e79d2fc6d84224c
Carmela <carmellush@gmail.com>
2024-12-29 19:55:40 +0200

    my-bookings component

---
+++ src/app/features/bookings/pages/my-bookings/my-bookings.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { BookingService } from '../../service/booking.service';
+import { FlightService } from '../../../flights/service/flight.service';
+import { Booking } from '../../model/booking.model';
+import { Flight } from '../../../flights/model/flight.model';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-my-bookings',
+  standalone: true,
+  imports: [CommonModule, RouterLink,],
+  templateUrl: './my-bookings.component.html',
+  styleUrls: ['./my-bookings.component.css']
+})
+export class MyBookingsComponent implements OnInit {
+  upcomingBookings: { booking: Booking; flight: Flight }[] = [];
+  previousBookings: { booking: Booking; flight: Flight }[] = [];
+
+  constructor(private bookingService: BookingService, private flightService: FlightService) {}
+
+  ngOnInit(): void {
+    const now = new Date();
+
+    const allBookings = this.bookingService.list();
+
+    // Split bookings into upcoming and previous
+    allBookings.forEach((booking) => {
+      const flight = this.flightService.get(booking.flightNumber);
+      if (flight) {
+        if (flight.boardingDate > now) {
+          this.upcomingBookings.push({ booking, flight });
+        } else {
+          this.previousBookings.push({ booking, flight });
+        }
+      }
+    });
+  }
+
+  hasUpcomingBookings(): boolean {
+    return this.upcomingBookings.length > 0;
+  }
+
+  hasPreviousBookings(): boolean {
+    return this.previousBookings.length > 0;
+  }
+}
