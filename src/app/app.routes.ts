b092325ae12d76acaa3fa6715e8997e38d083632
Carmela <carmellush@gmail.com>
2024-12-29 14:12:01 +0200

    add routes for viewing destinations

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,8 @@ import { FlightsComponent } from './features/flights/pages/flights/flights.compo
 import { EditFlightComponent } from './features/flights/pages/edit-flight/edit-flight.component';
 import { HelpPageComponent } from './features/help-page/help-page.component';
 import { DestinationsComponent } from './features/destinations/pages/destinations/destinations.component';
+import { ViewDestinationComponent } from './features/destinations/pages/view-destination/view-destination.component';
+import { MyBookingsComponent } from './features/bookings/pages/my-bookings/my-bookings.component';
 
 export const routes: Routes = [
     { path: '', component: HomePageComponent }, 
@@ -14,8 +16,10 @@ export const routes: Routes = [
     { path: 'flights', component: FlightsComponent },
     { path: 'edit-flight/:flightNumber', component: EditFlightComponent },
     { path: 'help', component: HelpPageComponent },
-    { path: '**', redirectTo: '' },
-    { path: 'destinations', component: DestinationsComponent },
+    { path: 'destinations', component: DestinationsComponent },
+    { path: 'view-destination/:code', component: ViewDestinationComponent },
+    { path: 'my-bookings', component: MyBookingsComponent },
+    { path: '**', redirectTo: '' }
 ];
 
 export const appRouterProviders = [
