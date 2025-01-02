c35aa7d4b0c55346c95a3c4c79a5144e863e81e8
Carmela <carmellush@gmail.com>
2025-01-02 21:59:41 +0200

    remove FlightsComponent and reuse FlightsTableComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { provideRouter, Routes } from '@angular/router';
 import { HomePageComponent } from './features/home-page/home-page.component';
 import { FlightsTableComponent } from './features/flights/pages/flights-table/flights-table.component';
 import { BookFlightComponent } from './features/flights/pages/book-flight/book-flight.component';
-import { FlightsComponent } from './features/flights/pages/flights/flights.component';
 import { EditFlightComponent } from './features/flights/pages/edit-flight/edit-flight.component';
 import { HelpPageComponent } from './features/help-page/help-page.component';
 import { DestinationsComponent } from './features/destinations/pages/destinations/destinations.component';
@@ -11,10 +10,10 @@ import { MyBookingsComponent } from './features/bookings/pages/my-bookings/my-bo
 
 export const routes: Routes = [
     { path: '', component: HomePageComponent }, 
-    { path: 'find-flight', component: FlightsTableComponent, data: { tableTitle: 'Book a Flight' } },
+    { path: 'find-flight', component: FlightsTableComponent},
     { path: 'book/:flightNumber', component: BookFlightComponent }, 
-    { path: 'flights', component: FlightsComponent },
     { path: 'edit-flight/:flightNumber', component: EditFlightComponent },
+    { path: 'flights', component: FlightsTableComponent },
     { path: 'help', component: HelpPageComponent },
     { path: 'destinations', component: DestinationsComponent },
     { path: 'view-destination/:code', component: ViewDestinationComponent },
--- src/app/features/flights/pages/flights-table/flights-table.component.ts
+++ src/app/features/flights/pages/flights-table/flights-table.component.ts
@@ -1,34 +1,35 @@
-import { Component, OnInit, ViewChild, Input } from '@angular/core';
-import { MatTableModule, MatTableDataSource } from '@angular/material/table';
-import { MatSort, MatSortModule } from '@angular/material/sort';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { FlightService } from '../../service/flight.service';
 import { Flight } from '../../model/flight.model';
 import { MatButtonModule } from '@angular/material/button';
-import { RouterModule, ActivatedRoute } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+import { MatSortModule } from '@angular/material/sort';
 
 @Component({
   selector: 'app-flights-table',
   standalone: true,
-  imports: [MatTableModule, MatSortModule, CommonModule, MatButtonModule, RouterModule],
+  imports: [MatButtonModule, MatIconModule, RouterLink, CommonModule, MatTableModule, MatSortModule],
   templateUrl: './flights-table.component.html',
   styleUrls: ['./flights-table.component.css']
 })
 
 export class FlightsTableComponent implements OnInit {
-  @Input() tableTitle: string = ''; 
+  isAdmin: boolean = false; 
+  tableTitle: string = '';
   flights!: MatTableDataSource<Flight>;
-  displayedColumns: string[] = ['flightNumber', 'origin', 'destination', 'boardingDate', 'arrivalDate', 'numberOfSeats','actions'];
-
-  @ViewChild(MatSort, { static: true }) sort!: MatSort;
+  displayedColumns: string[] = ['flightNumber', 'origin', 'destination', 'boardingDate', 'arrivalDate', 'numberOfSeats', 'actions'];
 
   constructor(private flightService: FlightService, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.route.data.subscribe(data => {this.tableTitle = data['tableTitle'] || 'Find a Flight';});
+    this.isAdmin = this.route.routeConfig?.path === 'flights'; // manager screen
+    this.tableTitle = this.isAdmin ? 'Manage Flights' : 'Book a Flight';
+
+    const allFlights = this.flightService.list();
     const futureFlights = this.flightService.getFutureFlights();
-    this.flights = new MatTableDataSource(futureFlights);
-    this.flights.sort = this.sort;
-  }  
-  
+    this.flights = new MatTableDataSource(this.isAdmin ? allFlights : futureFlights);
+  }
 }
